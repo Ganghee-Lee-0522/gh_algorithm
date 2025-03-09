f36723362b7dba40168fd15337d19e0d058fe448
LEE GANGHEE <gh_5226@naver.com>
2025-03-10 08:49:23 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- C++/프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ C++/프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    queue<int> d;
+    vector<int> due;
     
     for(int p = 0; p < progresses.size(); p++) {
         int cnt = 1;
@@ -12,18 +12,21 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
         cnt = (100 - progresses[p]) / speeds[p];
         if((100 - progresses[p]) % speeds[p]) cnt++;
         
-        d.push(cnt);
+        due.push_back(cnt);
     }
     
-    while(!d.empty()) {
-        int cnt = 1;
-        int prev = d.front(); d.pop();
-        
-        while(!d.empty() && d.front() <= prev) {
-            d.front(); d.pop();
+    int cnt = 1;
+    int prev = 0;
+    int idx = 1;
+    
+    while(idx < progresses.size()) {
+        while(idx < progresses.size() && due[idx] <= due[prev]) {
+            idx++;
             cnt++;
         }
         answer.push_back(cnt);
+        prev = idx;
+        cnt = 0;
     }
     return answer;
 }
