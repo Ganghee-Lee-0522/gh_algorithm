e949376bf33e157bf5ff38e8332d85d2c9a8c3cd
LEE GANGHEE <gh_5226@naver.com>
2025-03-10 08:32:08 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 3.65 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue<int> d;
+    
+    for(int p = 0; p < progresses.size(); p++) {
+        int cnt = 1;
+        
+        cnt = (100 - progresses[p]) / speeds[p];
+        if((100 - progresses[p]) % speeds[p]) cnt++;
+        
+        d.push(cnt);
+        cout << "p: " << progresses[p] << ", cnt: " << cnt << '\n';
+    }
+    
+    while(!d.empty()) {
+        int cnt = 1;
+        int prev = d.front(); d.pop();
+        
+        while(!d.empty() && d.front() <= prev) {
+            d.front(); d.pop();
+            cnt++;
+        }
+        answer.push_back(cnt);
+    }
+    return answer;
+}
