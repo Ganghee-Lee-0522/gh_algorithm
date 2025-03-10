dec9b330ae75ca625a61e919df189cb00a279688
LEE GANGHEE <gh_5226@naver.com>
2025-03-11 01:26:18 +0900

    [level 2] Title: 프로세스, Time: 0.04 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+using namespace std;
+
+bool comp(int i, int j) {
+    return i > j;
+}
+
+int solution(vector<int> priorities, int location) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int answer = 0;
+    // 큐에 프로세스를 (location, priorities) 쌍으로 삽입
+    // priorities 배열을 내림차순 정렬
+    // 큐를 돌며 cur의 priorities == priorities [i]이면 pop() 후 answer++, 불일치하면 다시 push()
+    // 만약 cur의 location == location이면 return answer;
+    queue<pair<int, int>> q;
+    for(int i = 0; i < priorities.size(); i++) {
+        q.push({i, priorities[i]});
+    }
+    sort(priorities.begin(), priorities.end(), comp);
+    int idx = 0;
+    
+    for(int idx = 0; idx < priorities.size(); idx++) {
+        pair<int, int> cur = q.front(); q.pop();
+        while(cur.Y != priorities[idx]) {
+            q.push(cur);
+            cur = q.front(); q.pop();
+        }
+        answer++;
+        if(cur.X == location) {
+            return answer;
+        }
+    }
+    return answer;
+}
