4c3a545fb1c1ac6e1cb0f6a9fd0dc0d551d5a775
LEE GANGHEE <gh_5226@naver.com>
2025-03-20 00:09:01 +0900

    [level 1] Title: 폰켓몬, Time: 1.11 ms, Memory: 4.34 MB -BaekjoonHub

--- C++/프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ C++/프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -6,8 +6,17 @@ int solution(vector<int> nums)
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    /* erase & unique 사용 풀이 */
+    // int answer = nums.size() / 2;
+    // sort(nums.begin(), nums.end());
+    // nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    // return answer < nums.size() ? answer : nums.size();
+    
+    /* set 사용 풀이 */
     int answer = nums.size() / 2;
-    sort(nums.begin(), nums.end());
-    nums.erase(unique(nums.begin(), nums.end()), nums.end());
-    return answer < nums.size() ? answer : nums.size();
+    set <int> s;
+    for(int e : nums) {
+        s.insert(e);
+    }
+    return answer < s.size() ? answer : s.size();
 }
