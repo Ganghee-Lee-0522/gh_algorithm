c7ddaae58617eee05d6b792ea46d034d7962b47c
LEE GANGHEE <gh_5226@naver.com>
2025-03-20 15:55:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 38.66 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string answer = "";
+    // unordered_multiset<string> ums에 참가자 insert
+    // ums에서 완주자를 삭제
+    // ums에 남은 선수의 이름을 return
+    unordered_multiset<string> ums(participant.begin(), participant.end());
+    for(string s : completion) {
+        ums.erase(ums.find(s));
+    }
+    answer = *ums.begin();
+    return answer;
+}
