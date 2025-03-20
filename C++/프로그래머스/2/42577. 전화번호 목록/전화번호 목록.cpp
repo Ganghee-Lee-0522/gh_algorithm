619f7926c4854be847e392912e65746522c7b8aa
LEE GANGHEE <gh_5226@naver.com>
2025-03-20 16:27:19 +0900

    [level 2] Title: 전화번호 목록, Time: 76.27 ms, Memory: 35.7 MB -BaekjoonHub

--- C++/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ C++/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -9,7 +9,14 @@ bool solution(vector<string> phone_book) {
     // i번째와 i+1번째 find 비교하여 찾으면 return false
     sort(phone_book.begin(), phone_book.end());
     for(int i = 0; i < phone_book.size() - 1; i++) {
-        if(phone_book[i + 1].find(phone_book[i]) == 0) { // 접두어임
+        
+        /* 아래 코드는 접두어가 아니어도 동작하기 때문에 옳지 않음 */
+        // if(phone_book[i + 1].find(phone_book[i]) == 0) { // 접두어임
+        //     return false;
+        // }
+        
+        /* 무조건 접두어일 때만 작동하도록 수정 */
+        if(phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size())) {
             return false;
         }
     }
