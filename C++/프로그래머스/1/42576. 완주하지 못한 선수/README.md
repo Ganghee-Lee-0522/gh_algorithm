53b9cdbef9e9f9f60887dc990b4814a9dcf0e1c3
LEE GANGHEE <gh_5226@naver.com>
2025-04-16 01:35:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 34.53 ms, Memory: 33.1 MB -BaekjoonHub

--- C++/프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ C++/프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 33 MB, 시간: 35.69 ms
+메모리: 33.1 MB, 시간: 34.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 15:55:29
+2025년 04월 16일 01:35:50
 
 ### 문제 설명
 
--- C++/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ C++/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    ios::sync_with_stdio(0);
     cin.tie(0);
+    ios::sync_with_stdio(0);
     
-    string answer = "";
-    // unordered_multiset<string> ums에 참가자 insert
-    // ums에서 완주자를 삭제
-    // ums에 남은 선수의 이름을 return
-    unordered_multiset<string> ums(participant.begin(), participant.end());
-    for(string s : completion) {
-        ums.erase(ums.find(s)); // ums.erase(s)를 쓰면 중복 포함 모두 삭제 됨!
+    unordered_multiset<string> set(participant.begin(), participant.end());
+    for(string& c : completion) {
+        auto it = set.find(c);
+        if(it != set.end()) {
+            set.erase(it);
+        }
     }
-    answer = *ums.begin();
-    return answer;
+    
+    return *set.begin();
 }
