46ef4433e37d84fea26cf86b6f239a2c9d777f4c
LEE GANGHEE <gh_5226@naver.com>
2025-04-17 00:28:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.19 ms, Memory: 96.8 MB -BaekjoonHub

--- Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -6,19 +6,16 @@ class Solution {
         for(String s : participant) {
             map.compute(s, (k, v) -> v == null ? 1 : v + 1);
         }
-        
         for(String s : completion) {
-            int cnt = map.get(s);
-            if(cnt > 1) {
+            if(map.get(s) > 1) {
                 map.put(s, map.get(s) - 1);
             }
             else {
                 map.remove(s);
             }
         }
-        
-        for(String key : map.keySet()) {
-            return key;
+        for(String s : map.keySet()) {
+            return s;
         }
         
         return "";
