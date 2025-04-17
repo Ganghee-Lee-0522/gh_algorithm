6fe0c65a7308620ffa4f3c77dce68aca05ab2fc3
LEE GANGHEE <gh_5226@naver.com>
2025-04-17 14:15:36 +0900

    [level 1] Title: 모의고사, Time: 0.71 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Pair {
+    int x, y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] boardA = {5, 1, 2, 3, 4};
+        int[] boardB = {5, 2, 1, 2, 3, 2, 4, 2};
+        int[] boardC = {5, 3, 3, 1, 1, 2, 2, 4, 4, 5};
+        int[] cnt = new int[4];
+        List<Integer> answerList = new ArrayList<>();
+        
+        for(int i = 1; i <= answers.length; i++) {
+            if(answers[i - 1] == boardA[i % 5]) {
+                cnt[1]++;
+            }
+            if(answers[i - 1] == boardB[i % 8]) {
+                cnt[2]++;
+            }
+            if(answers[i - 1] == boardC[i % 10]) {
+                cnt[3]++;
+            }
+        }
+        
+        if(cnt[1] > cnt[2] && cnt[1] > cnt[3]) {
+            cnt[0] = cnt[1];
+        }
+        else if(cnt[2] > cnt[3]) {
+            cnt[0] = cnt[2];
+        }
+        else {
+            cnt[0] = cnt[3];
+        }
+        
+        int idx = 0;
+        
+        for(int i = 1; i <= 3; i++) {
+            if(cnt[i] == cnt[0]) {
+                answerList.add(i);
+            }
+        }
+        int[] answer = new int[answerList.size()];
+        for(int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+        
+        return answer;
+    }
+}
