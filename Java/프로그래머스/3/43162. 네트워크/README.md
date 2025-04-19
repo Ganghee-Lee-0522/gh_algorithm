ce6ff8cb1673bf550500dcc4b0d4977ff9d8a4f0
LEE GANGHEE <gh_5226@naver.com>
2025-04-19 23:18:46 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 84.4 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/README.md
+++ Java/프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 81.3 MB, 시간: 0.30 ms
+메모리: 84.4 MB, 시간: 0.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 19일 21:56:26
+2025년 04월 19일 23:18:42
 
 ### 문제 설명
 
--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -4,8 +4,8 @@ class Solution {
     boolean[] visited;
     
     public int solution(int n, int[][] computers) {
-        int answer = 0;
         visited = new boolean[n];
+        int answer = 0;
         
         for(int i = 0; i < computers.length; i++) {
             if(!visited[i]) {
@@ -19,7 +19,6 @@ class Solution {
     
     public void dfs(int cur, int[][] computers) {
         visited[cur] = true;
-        
         for(int nxt = 0; nxt < computers.length; nxt++) {
             if(computers[cur][nxt] == 1 && !visited[nxt]) {
                 dfs(nxt, computers);
