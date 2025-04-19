3521d5adfdc7ed69239b41fc02353c4ff6f6c587
LEE GANGHEE <gh_5226@naver.com>
2025-04-19 20:55:18 +0900

    [level 2] Title: 타겟 넘버, Time: 6.03 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    int count = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return count;
+    }
+    
+    public void dfs(int[] numbers, int target, int depth, int sum) {
+        if(depth == numbers.length) {
+            if(target == sum) {
+                count++;
+            }
+            return;
+        }
+        
+        dfs(numbers, target, depth + 1, sum + numbers[depth]);
+        dfs(numbers, target, depth + 1, sum - numbers[depth]);
+    }
+}
