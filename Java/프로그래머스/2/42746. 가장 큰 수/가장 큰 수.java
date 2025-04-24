1dd263f192afd0e80d4da651fa121b71e64bc294
LEE GANGHEE <gh_5226@naver.com>
2025-04-24 13:25:58 +0900

    [level 2] Title: 가장 큰 수, Time: 275.32 ms, Memory: 190 MB -BaekjoonHub

--- Java/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ Java/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
         Integer[] boxed = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
         Arrays.sort(boxed, (a, b) -> (b.toString() + a.toString()).compareTo(a.toString() + b.toString()));
-        StringBuilder sb = new StringBuilder();
         for(int num : boxed) {
             sb.append(num);
         }
+        
         if(sb.charAt(0) == '0') {
             return "0";
         }
