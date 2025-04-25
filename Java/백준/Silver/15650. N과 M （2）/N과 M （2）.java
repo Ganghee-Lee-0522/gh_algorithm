276d6c7e6a9547161c99c52ba51050402a25e990
LEE GANGHEE <gh_5226@naver.com>
2025-04-25 22:23:22 +0900

    [Silver III] Title: N과 M (2), Time: 176 ms, Memory: 17700 KB -BaekjoonHub

--- Java/백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ Java/백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -5,38 +5,39 @@ public class Main
     static int N, M;
     static boolean[] visited;
     static int[] answer;
+    static StringBuilder sb = new StringBuilder();
     
 	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    N = sc.nextInt();
-	    M = sc.nextInt();
-	    
-	    visited = new boolean[N + 1];
-	    answer = new int[M];
-	    
-	    dfs(1, 0);
-	    
-	    return;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        
+        visited = new boolean[N + 1];
+        answer = new int[M];
+        dfs(0, 1);
+        System.out.print(sb);
+        
+        return;
 	}
-	
-	public static void dfs(int cur, int depth) {
-	    if(depth == M) {
-	        for(int i : answer) {
-	            System.out.print(i + " ");
-	        }
-	        System.out.println();
-	        return;
-	    }
-	    
-	    for(int idx = cur; idx <= N; idx++) {
-	        if(!visited[idx]) {
-	            visited[idx] = true;
-	            answer[depth] = idx;
-	            dfs(idx + 1, depth + 1);
-	            visited[idx] = false;
-	        }
-	    }
-	    
-	    return;
-	}
+    
+    public static void dfs(int depth, int cur) {
+        if(depth == M) {
+            for(int i : answer) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for(int idx = cur; idx <= N; idx++) {
+            if(!visited[idx]) {
+                visited[idx] = true;
+                answer[depth] = idx;
+                dfs(depth + 1, idx + 1);
+                visited[idx] = false;
+            }
+        }
+        
+        return;
+    }
 }
