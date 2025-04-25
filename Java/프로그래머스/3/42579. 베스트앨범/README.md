3df92a3e3ffd91b1bce4e2a2a6af8100c8183ff9
LEE GANGHEE <gh_5226@naver.com>
2025-04-25 11:36:34 +0900

    [level 3] Title: 베스트앨범, Time: 4.36 ms, Memory: 84.8 MB -BaekjoonHub

--- Java/프로그래머스/3/42579. 베스트앨범/README.md
+++ Java/프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 82.8 MB, 시간: 10.96 ms
+메모리: 84.8 MB, 시간: 4.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 14:30:05
+2025년 04월 25일 11:36:29
 
 ### 문제 설명
 
--- Java/프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ Java/프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -10,33 +10,40 @@ class Song {
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> genreCount = new HashMap<>();
-        Map<String, List<Song>> playList = new HashMap<>();
         List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> genreList = new HashMap<>(); // 장르별 총 재생 수
+        Map<String, List<Song>> songList = new HashMap<>(); // 고유 번호별 재생 수
         
+        // 장르별 총 재생 수를 저장
+        // 고유 번호별 재생 수를 저장
         for(int i = 0; i < genres.length; i++) {
             String genre = genres[i];
             int play = plays[i];
             
-            genreCount.compute(genre, (a, b) -> b == null ? play : b + play);
-            playList.putIfAbsent(genre, new ArrayList<>());
-            playList.get(genre).add(new Song(i, play));
+            // 장르별 총 재생 수 update
+            genreList.put(genre, genreList.getOrDefault(genre, 0) + play);
+            // 고유 번호별 재생 수 save
+            songList.putIfAbsent(genre, new ArrayList<>());
+            songList.get(genre).add(new Song(i, play));
         }
         
-        List<String> sortedGenre = new ArrayList<>(genreCount.keySet());
-        sortedGenre.sort((a, b) -> genreCount.get(b) - genreCount.get(a));
+        // 장르별 총 재생 수 내림차순 정렬
+        List<String> sortedGenre = new ArrayList<>(genreList.keySet());
+        sortedGenre.sort((a, b) -> genreList.get(b).compareTo(genreList.get(a)));
         
+        // 장르별 재생 수 내림차순(or idx 오름차순) 정렬, 최다 재생 곡 (상위 2개) answer에 저장
         for(String genre : sortedGenre) {
-            List<Song> songList = playList.get(genre);
-            songList.sort((a, b) -> {
-                if(a.play == b.play) {
+            List<Song> sortedPlay = new ArrayList<>(songList.get(genre));
+            
+            sortedPlay.sort((a, b) -> {
+                if(b.play == a.play) {
                     return a.idx - b.idx;
                 }
                 return b.play - a.play;
             });
             
-            for(int i = 0; i < Math.min(2, songList.size()); i++) {
-                answer.add(songList.get(i).idx);
+            for(int i = 0; i < Math.min(2, sortedPlay.size()); i++) {
+                answer.add(sortedPlay.get(i).idx);
             }
         }
         
